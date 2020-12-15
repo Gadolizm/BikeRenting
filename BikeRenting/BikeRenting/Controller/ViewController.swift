6ad964164e9649d4dccb849987020b9c130533e1
Haitham Gado <haitham.gado@naqla.org>
2020-12-16 01:27:05 +0200

    logic

--- BikeRenting/BikeRenting/Controller/ViewController.swift
+++ BikeRenting/BikeRenting/Controller/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import CoreData
 
+
 class ViewController: UIViewController {
     
     // MARK:- Outlets
@@ -19,8 +20,10 @@ class ViewController: UIViewController {
     // MARK:- Properties
     
     var bikes: [NSManagedObject] = []
-    let initiatedNames = ["Bike 1","Bike 2", "Bike 3", "Bike 4" ,"Bike 5","Bike 6","Bike 7","Bike 8"]
-    var selectedBike = ""
+    let initiatedNames = [(0,"Bike 1"),(1,"Bike 2"), (2,"Bike 3"), (3,"Bike 4") ,(4,"Bike 5"),(5,"Bike 6"),(6,"Bike 7"),(7,"Bike 8")]
+//    let initiatedNames = ["Bike 1","Bike 2", "Bike 3", "Bike 4" ,"Bike 5","Bike 6","Bike 7","Bike 8"]
+
+    var selectedBike : (id: Int, name: String)?
     
     
     // MARK:- Override Functions
@@ -43,10 +46,11 @@ class ViewController: UIViewController {
         do {
             bikes = try managedContext.fetch(fetchRequest)
             if bikes.count == 0 {
-                initiatedNames.forEach{save(name: $0)}
-                self.tableView.reloadData()
+                initiatedNames.forEach{save(id: $0.0,name: $0.1)}
 
             }
+            self.tableView.reloadData()
+
         } catch let error as NSError {
             print("Could not fetch. \(error), \(error.userInfo)")
         }
@@ -65,31 +69,44 @@ class ViewController: UIViewController {
     
     @IBAction func saveButtonTapped(_ sender: Any) {
         
-        if selectedBike == "" {
-            alertDisplay()
+        if let selected = selectedBike {
+
+            guard let rentingHours = Int(rentingHoursTextField.text ?? "") else { return }
+            update(bike: BikeEntity(id: selected.id, name: selected.name, availability: false, rentFrom: getDate(hours: rentingHours).startingDate, rentTo: getDate(hours: rentingHours).endingDate))
+            print(bikes[selectedBike?.id ?? 0])
         } else {
+            alertNotSelectDisplay()
             
         }
+        tableView.reloadData()
     }
     
     // MARK:- Methods
     
     
-    func update(name: String)  {
+    func update(bike: BikeEntity)  {
         
         
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
-        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Alert")
+//        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Bike")
 
-        fetchRequest.predicate = NSPredicate(format: "name = %@", name)
+        let fetchRequest:NSFetchRequest<NSFetchRequestResult> = NSFetchRequest.init(entityName: "Bike")
+        let predicate = NSPredicate(format: "id = '\(bike.id)'")
+        fetchRequest.predicate = predicate
+        
+//        fetchRequest.predicate = NSPredicate(format: "id = %@", bike.id)
 
         do {
             let results = try context.fetch(fetchRequest) as? [NSManagedObject]
             if results?.count != 0 { // Atleast one was returned
 
                 // In my case, I only updated the first item in results
-                results?[0].setValue("yourValueToBeSet", forKey: "yourCoreDataAttribute")
+                results?[0].setValue(bike.availability, forKey: "availability")
+                results?[0].setValue(bike.rentFrom, forKey: "rentFrom")
+                results?[0].setValue(bike.rentTo, forKey: "rentTo")
+
+
             }
         } catch {
             print("Fetch Failed: \(error)")
@@ -106,11 +123,20 @@ class ViewController: UIViewController {
   
         
     }
+
+    func getDate(hours: Int)-> (startingDate: Date, endingDate: Date) {
+        let startDate = Date()
+        let timeFormatter = DateFormatter()
+        timeFormatter.dateFormat = "HH:ss"
+//        let stringDate = timeFormatter.string(from: time)
+        let calendar = Calendar.current
+        guard let endDate = calendar.date(byAdding: .hour, value: hours, to: startDate) else { return (Date(),Date()) }
+//        let endingDate = timeFormatter.string(from: endDate)
+        return (startDate,endDate)
+    }
     
-    func alertDisplay()  {
-        let alert = UIAlertController(title: "Alert",
-                                      message: "Select Bike before pressing save please",
-                                      preferredStyle: .alert)
+    func alertNotSelectDisplay()  {
+        let alert = UIAlertController(title: "Alert", message: "Select Bike before pressing save please", preferredStyle: .alert)
         
         
         
@@ -123,7 +149,21 @@ class ViewController: UIViewController {
         present(alert, animated: true)
     }
     
-    func save(name: String) {
+    func alertNotAvailableDisplay()  {
+        let alert = UIAlertController(title: "Alert", message: "Select available bike please", preferredStyle: .alert)
+        
+        
+        
+        let okAction = UIAlertAction(title: "Ok", style: .default)
+
+        
+        
+        alert.addAction(okAction)
+        
+        present(alert, animated: true)
+    }
+    
+    func save(id: Int, name: String) {
         
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
             return
@@ -138,7 +178,9 @@ class ViewController: UIViewController {
         let bike = NSManagedObject(entity: entity, insertInto: managedContext)
         
         // 3
+        bike.setValue(id, forKeyPath: "id")
         bike.setValue(name, forKeyPath: "name")
+
         
         // 4
         do {
@@ -165,9 +207,10 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
 
         let currentCell = tableView.cellForRow(at: indexPath!)! as UITableViewCell
 
-
-        selectedBike = currentCell.textLabel?.text ?? ""
-        print(selectedBike)
+        if currentCell.backgroundColor == #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1) {
+            alertNotAvailableDisplay()
+        }
+        selectedBike = (Int(indexPath!.row), currentCell.textLabel?.text ?? "")
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -175,6 +218,11 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
         let bike = bikes[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell",for: indexPath)
         cell.textLabel?.text = bike.value(forKeyPath: "name") as? String
+        guard let availability = bike.value(forKeyPath: "availability") as? Bool else { return  cell}
+        if  availability == false {
+            cell.backgroundColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+            cell.selectionStyle = .none
+        }
         return cell
     }
 }
---
+++ BikeRenting/BikeRenting/Model/BikeEntity.swift
@@ -0,0 +1,17 @@
+//
+//  BikeEntity.swift
+//  BikeRenting
+//
+//  Created by Gado on 15/12/2020.
+//
+
+import Foundation
+
+
+struct BikeEntity {
+    var id : Int
+    var name : String
+    var availability : Bool
+    var rentFrom : Date
+    var rentTo : Date
+}
